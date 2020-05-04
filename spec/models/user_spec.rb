8d40d3d9bd0cb8d54d41defb6d20fb562706467d
letItCurl <rolandlopez.developer@gmail.com>
2020-05-04 15:55:20 +0200

    full_name method + test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+    before do
+        @user = User.create(email: "test@mail.com", password:"123456789", password_confirmation:"123456789", first_name: "jhon", last_name:"snow")
+    end
+
     describe "Creation" do
-        before do
-            @user = User.create(email: "test@mail.com", password:"123456789", password_confirmation:"123456789", first_name: "jhon", last_name:"snow")
-        end
         it "can be created" do
             expect(@user).to be_valid
         end
@@ -14,4 +15,11 @@ RSpec.describe User, type: :model do
             expect(@user).to_not be_valid
         end
     end
+
+    describe "custom name methods" do
+        it "has a full name method" do
+           expect(@user.full_name).to eq("SNOW, JHON") 
+        end
+    end
+
 end
