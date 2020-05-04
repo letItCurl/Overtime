f41e1321b4b76f0b300b918c302137990dfc8a10
letItCurl <rolandlopez.developer@gmail.com>
2020-05-04 15:35:13 +0200

    html table posts

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,10 +1,14 @@
 require 'rails_helper'
 
 describe 'navigate to' do
-  describe 'index:' do
-    before do
+
+    before do 
         @user = User.create(email: "test@mail.com", password:"123456789", password_confirmation:"123456789", first_name: "jhon", last_name:"snow")
         login_as(@user, :scope => :user)
+    end
+
+  describe 'index:' do
+    before do
         visit posts_path
     end
     it "can be reached" do
@@ -13,12 +17,16 @@ describe 'navigate to' do
     it "has a title of Posts" do
         expect(page).to have_content(/Posts/)
     end
+    it "has a list of posts" do 
+        p1 = Post.create(date: Date.today, rationale: "p1", user_id: @user.id)
+        p2 = Post.create(date: Date.today, rationale: "p2", user_id: @user.id)
+        visit posts_path # ORDER IS IMPORTANT DUMBASS
+        expect(page).to have_content(/p1|p2/)
+    end
   end
 
   describe 'creation' do
     before do
-        @user = User.create(email: "test@mail.com", password:"123456789", password_confirmation:"123456789", first_name: "jhon", last_name:"snow")
-        login_as(@user, :scope => :user)
         visit new_post_path
     end
     it "has a new form that can be reached" do
