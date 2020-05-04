95105264506a6afdb0b44f6ef8c0afd5da32ca51
letItCurl <rolandlopez.developer@gmail.com>
2020-05-04 14:23:29 +0200

    Protecting user reach + test

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,18 +2,23 @@ require 'rails_helper'
 
 describe 'navigate to' do
   describe 'index:' do
-    it "can be reached" do
+    before do
+        @user = User.create(email: "test@mail.com", password:"123456789", password_confirmation:"123456789", first_name: "jhon", last_name:"snow")
+        login_as(@user, :scope => :user)
         visit posts_path
+    end
+    it "can be reached" do
         expect(page.status_code).to eq(200)
     end
     it "has a title of Posts" do
-        visit posts_path
         expect(page).to have_content(/Posts/)
     end
   end
 
   describe 'creation' do
     before do
+        @user = User.create(email: "test@mail.com", password:"123456789", password_confirmation:"123456789", first_name: "jhon", last_name:"snow")
+        login_as(@user, :scope => :user)
         visit new_post_path
     end
     it "has a new form that can be reached" do
@@ -24,6 +29,12 @@ describe 'navigate to' do
         fill_in 'post[rationale]', with: "Some rationale"
         click_on "Save"
         expect(page).to have_content("Some rationale")
+    end
+    it "will have a user associated" do
+        fill_in 'post[date]', with: Date.today
+        fill_in 'post[rationale]', with: "User_Association"
+        click_on "Save"
+        expect(User.last.posts.last.rationale).to eq("User_Association")
     end
 end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   describe "Creation" do
     before do
-      @post = Post.create(date: Date.today, rationale: "Anything")
+      @user = User.create(email: "test@mail.com", password:"123456789", password_confirmation:"123456789", first_name: "jhon", last_name:"snow")
+      @post = Post.create(date: Date.today, rationale: "Anything", user_id: @user.id)
+      login_as(@user, :scope => :user)
     end
     it "can be created" do
       expect(@post).to be_valid 
