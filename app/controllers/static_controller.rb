f67ec0f843217967deb12336baca12eae6dfc765
letItCurl <rolandlopez.developer@gmail.com>
2020-05-08 18:10:32 +0200

    adding audit log to homepage for admins

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,6 @@
 class StaticController < ApplicationController
   def homepage
-    @pending_approvals = Post.where(status: 'submitted')
-    @pending_approvals = Post.where(status: 'submitted')
+    @pending_approvals = Post.where(status: 'submitted').first(12)
+    @recent_audit_items = AuditLog.first(12)
   end
 end
