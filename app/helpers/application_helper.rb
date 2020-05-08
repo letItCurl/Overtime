e2b6601da445de9381190c7a87676cb1b18317d8
letItCurl <rolandlopez.developer@gmail.com>
2020-05-08 12:46:50 +0200

    helper status_label updated

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
     def active?(path)
         "active" if current_page?(path)
     end
+    def status_label status
+        tag = {
+        "approved" => content_tag(:span, status, class: "label label-success"),
+        "submitted" => content_tag(:span, status, class: "label label-primary"),
+        "rejected" => content_tag(:span, status, class: "label label-danger"),
+        "confirmed" => content_tag(:span, status, class: "label label-success"),
+        "pending" => content_tag(:span, status, class: "label label-warning"),
+        }
+        tag[status]
+    end
 end
