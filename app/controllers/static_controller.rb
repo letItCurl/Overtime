25cf64c4800bab884b14a00feb32a709c9b2562d
letItCurl <rolandlopez.developer@gmail.com>
2020-05-11 12:31:34 +0200

    employee styling

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,11 @@
 class StaticController < ApplicationController
   def homepage
-    @pending_approvals = Post.where(status: 'submitted').first(12)
-    @recent_audit_items = AuditLog.first(12)
+
+    if admin_types.include?(current_user.type)
+      @pending_approvals = Post.submitted.first(12)
+      @recent_audit_items = AuditLog.first(12)
+    else
+      @pending_audit_confirmations = current_user.audit_logs.first(5)
+    end
   end
 end
